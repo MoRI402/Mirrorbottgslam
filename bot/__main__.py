0d76a99e3ef727366d4a1f0ebaea930284df99f6
Siddharth Kumar <79321201+MoRI402@users.noreply.github.com>
2021-11-27 18:17:00 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -45,8 +45,8 @@ def stats(update, context):
 
 def start(update, context):
     buttons = button_build.ButtonMaker()
-    buttons.buildbutton("Repo", "https://github.com/SlamDevs/slam-mirrorbot")
-    buttons.buildbutton("Channel", "https://t.me/SlamMirrorUpdates")
+    buttons.buildbutton("Made By", "https://t.me/Siddharth_Otaku")
+    buttons.buildbutton("Channel", "https://t.me/Animax_Industry")
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
